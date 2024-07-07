8f2ef1c7eb799977fa0f5ed000133e44cc80d3af
Monika Grzanek <grzanekmonika@o2.pl>
2024-07-07 13:45:05 +0200

    Add phoneNumber array and add phone and email properties to person object

---
+++ app.js
@@ -0,0 +1,37 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable no-undef */
+const fs = require('fs');
+
+const genders = ['F', 'M'];
+const maleNames = ['Adam', 'Michael', 'Peter', 'John', 'Tom', 'Martin'];
+const femaleNames = ['Monica', 'Eveline', 'Emily', 'Miley', 'Bella', 'Diana'];
+const lastNames = ['Smith', 'Jordan', 'Pollo', 'Doe', 'Wilson', 'Rayan', 'Parker', 'Kingston'];
+const phoneNumbers = ['607-690-543', '696-592-002', '607-892-345', '789-065-231', '510-765-390'];
+
+const randChoice = arr => {
+    return arr[Math.floor(Math.random() * arr.length)]
+}
+
+const people = [];
+
+for(let i = 0; i < 20; i++) {
+    const person = {};
+    person.gender = randChoice(genders);
+    if(person.gender === 'F') {
+        person.firstName = randChoice(femaleNames);
+    } else {
+        person.firstName = randChoice(maleNames);
+    }
+    person.lastNames = lastNames[Math.floor(Math.random() * lastNames.length)];
+    person.age = Math.floor((Math.random() * 60) + 18);
+    person.phone = phoneNumbers[Math.floor(Math.random() * phoneNumbers.length)];
+    person.email = `${person.firstName}.${person.lastName}@example.com`;
+
+    people.push(person);
+}
+
+fs.writeFile('people.json', JSON.stringify(people), (err) => {
+    if (err) throw err;
+    console.log('The file has been saved!');
+    console.log(people);
+  });     
